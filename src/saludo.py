b70ee036fa476835049bec50f3a6dea42175c81b
Mario <marlopfer2@alum.us.es>
2026-09-17 14:56:56 +0200

    Sin terminar

---
+++ src/saludo.py
@@ -0,0 +1,6 @@
+input('Ingrese su nombre:')
+def hora_actual = datetime.now().hour
+from datetime 0 to datetime 12: pretty_print(Buenos dias, 'NOMBRE')
+from datetime 12 to datetime 21: pretty_print(Buenas tardes, 'NOMBRE')
+from datetime 21 to datetime 23: pretty_print(Buenas noches, 'NOMBRE')
+
